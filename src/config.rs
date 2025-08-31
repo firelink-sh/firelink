4118f90611b1f00784faa9711c1373d5e327c9fc
Wilhelm Ågren <wilhelmagren98@gmail.com>
2025-08-31 17:43:13 +0200

    simple config parsing

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,32 @@
 use serde::Deserialize;
+use std::collections::HashMap;
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum RunIf {
+    AllSuccess,
+    AnySuccess,
+    Always,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum Trigger {
+    Cron { schedule: String },
+    AfterDeps { condition: RunIf },
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum Executor {
+    Bash {
+        script: String,
+        #[serde(default)]
+        args: Option<Vec<String>>,
+        #[serde(default)]
+        env: Option<HashMap<String, String>>,
+    },
+}
 
 #[derive(Debug, Deserialize, PartialEq)]
 pub struct Job {
@@ -6,4 +34,18 @@ pub struct Job {
     pub depends_on: Option<Vec<String>>,
     pub trigger: Trigger,
     pub executor: Executor,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub jobs: Vec<Job>,
+}
+
+impl Config {
+    pub fn from_yaml_str(yaml: &str) -> Self {
+        Config::try_from_yaml_str(yaml).unwrap()
+    }
+    pub fn try_from_yaml_str(yaml: &str) -> Result<Self, serde_yaml::Error> {
+        serde_yaml::from_str(yaml)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod config;
+pub use config::Config;
 
 #[tokio::main]
 async fn main() {
     println!("heyo");
+    let c = Config::from_yaml_str("jobs:");
+    println!("{:?}", c);
 }
